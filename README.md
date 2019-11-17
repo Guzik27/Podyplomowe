c9c45b5b7f0de4cc0d6aed29f8c269504d37d1ce
Guzik27 <dguzikow27@gmail.com>
2019-11-17 08:49:24 +0100

    Dodanie zdjęcia

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Podyplomowe
 // Program wyświetla napis "Dzień dobry:)"
+
+
+![alt text](https://cdn01.dziennikwschodni.pl/media/news/2019/2019-02/84a2f4213fd677f24d0695487099a89f_org_336.jpg "Zdjęcie ")
--- src/Podyplomowe.java
+++ src/Podyplomowe.java
@@ -1,5 +1,7 @@
 public class Podyplomowe {
     public static void main(String[] args) {
         System.out.println("Dzień dobry :)");
+
+        // jest to program wyświetlający napis "Dzień dobry :)"
     }
 }
